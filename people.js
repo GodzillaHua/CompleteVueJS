4c0e2269d928977b54448ce935fe4a6b07bc4f0a
hua wei <godzilla@MacBook-Pro.local>
2020-06-16 17:39:17 +0800

    first commit

---
+++ people.js
@@ -0,0 +1,317 @@
+const people = [
+    {
+        "_id": "5ee88003ef72037785758fee",
+        "index": 0,
+        "guid": "84d68df8-50ad-4fd9-91b1-a82d5663e917",
+        "isActive": false,
+        "balance": "$1,085.68",
+        "picture": "http://placehold.it/32x32",
+        "age": 31,
+        "eyeColor": "brown",
+        "name": "Ingrid Jimenez",
+        "gender": "female",
+        "company": "OPTICON",
+        "email": "ingridjimenez@opticon.com",
+        "phone": "+1 (904) 541-3324",
+        "address": "787 Johnson Avenue, Cataract, Georgia, 5764",
+        "about": "In ad nulla non cupidatat pariatur sit voluptate ea. Reprehenderit nulla cillum ullamco elit id dolore nisi enim dolor aliquip. Exercitation do veniam eu consectetur aute.\r\n",
+        "registered": "2018-09-16T09:57:19 -08:00",
+        "latitude": -46.100361,
+        "longitude": -28.12363,
+        "tags": [
+            "mollit",
+            "nisi",
+            "nisi",
+            "id",
+            "cillum",
+            "est",
+            "adipisicing"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Gomez Sandoval"
+            },
+            {
+                "id": 1,
+                "name": "Alma Chen"
+            },
+            {
+                "id": 2,
+                "name": "Reid Benson"
+            }
+        ],
+        "greeting": "Hello, Ingrid Jimenez! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ee8800392b3a12b34cda67a",
+        "index": 1,
+        "guid": "8e22dc82-0772-4a18-9be3-8d4a53c5ccc1",
+        "isActive": false,
+        "balance": "$1,307.68",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "brown",
+        "name": "Patricia Bass",
+        "gender": "female",
+        "company": "DRAGBOT",
+        "email": "patriciabass@dragbot.com",
+        "phone": "+1 (809) 409-3955",
+        "address": "226 Aberdeen Street, Welch, North Carolina, 8521",
+        "about": "Culpa laboris et tempor ipsum non deserunt cupidatat. Aute veniam ullamco irure id ut consectetur consectetur commodo qui ex aute occaecat ea adipisicing. Sint dolor voluptate irure pariatur eiusmod non laboris eu. Commodo adipisicing esse non ex aliquip aliqua occaecat excepteur quis enim mollit est est do. Duis pariatur aliqua magna duis do magna in elit enim excepteur qui cillum. Tempor enim sint sit qui. Magna et nostrud ullamco ea occaecat quis in officia quis aute Lorem veniam.\r\n",
+        "registered": "2016-03-10T10:22:47 -08:00",
+        "latitude": -66.853875,
+        "longitude": 160.823033,
+        "tags": [
+            "exercitation",
+            "officia",
+            "ut",
+            "proident",
+            "velit",
+            "sit",
+            "do"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Yates Sears"
+            },
+            {
+                "id": 1,
+                "name": "Ray Atkinson"
+            },
+            {
+                "id": 2,
+                "name": "Johnston Contreras"
+            }
+        ],
+        "greeting": "Hello, Patricia Bass! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ee8800387897520d0653c9a",
+        "index": 2,
+        "guid": "5857a77c-97bc-4c83-8eba-c730f8f17e87",
+        "isActive": false,
+        "balance": "$2,648.45",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "blue",
+        "name": "Megan Pate",
+        "gender": "female",
+        "company": "EMERGENT",
+        "email": "meganpate@emergent.com",
+        "phone": "+1 (997) 489-3130",
+        "address": "290 Ditmas Avenue, Gibsonia, South Dakota, 1441",
+        "about": "Est reprehenderit est labore incididunt exercitation irure eu est deserunt aliquip eu adipisicing sit. Cillum eu Lorem amet culpa. Dolor Lorem non anim voluptate aliqua incididunt consequat enim ad esse ex fugiat eiusmod. Sit labore anim nisi cupidatat tempor proident veniam. Laboris consequat voluptate eu laborum Lorem amet laboris sint adipisicing incididunt reprehenderit.\r\n",
+        "registered": "2016-04-25T12:22:01 -08:00",
+        "latitude": -83.999841,
+        "longitude": 172.624685,
+        "tags": [
+            "consequat",
+            "adipisicing",
+            "non",
+            "eiusmod",
+            "aliquip",
+            "aliqua",
+            "mollit"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Cooper Russell"
+            },
+            {
+                "id": 1,
+                "name": "Maritza Hays"
+            },
+            {
+                "id": 2,
+                "name": "Whitehead Hart"
+            }
+        ],
+        "greeting": "Hello, Megan Pate! You have 10 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5ee88003419ca7077db15393",
+        "index": 3,
+        "guid": "e6c2b325-b1f3-442e-b344-eb74ec8f593a",
+        "isActive": true,
+        "balance": "$1,169.79",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "blue",
+        "name": "Quinn Mosley",
+        "gender": "male",
+        "company": "KOZGENE",
+        "email": "quinnmosley@kozgene.com",
+        "phone": "+1 (813) 426-3425",
+        "address": "130 Lawrence Avenue, Orick, Utah, 760",
+        "about": "Consequat ipsum enim cupidatat in. Ut Lorem commodo ut dolor sint aute exercitation aliquip culpa duis dolor. Laborum commodo aute ex tempor sit qui amet. Esse pariatur ad irure eiusmod ipsum deserunt sit ex esse. Excepteur pariatur id dolor do reprehenderit voluptate id ad irure ea.\r\n",
+        "registered": "2016-08-11T01:08:06 -08:00",
+        "latitude": -44.363401,
+        "longitude": -19.928614,
+        "tags": [
+            "laborum",
+            "quis",
+            "commodo",
+            "voluptate",
+            "fugiat",
+            "fugiat",
+            "est"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Kathryn Gamble"
+            },
+            {
+                "id": 1,
+                "name": "Harper Poole"
+            },
+            {
+                "id": 2,
+                "name": "Cash Monroe"
+            }
+        ],
+        "greeting": "Hello, Quinn Mosley! You have 3 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ee88003f561dce42c731c66",
+        "index": 4,
+        "guid": "aa1f07d4-8afe-4459-b3c2-a5286510df39",
+        "isActive": true,
+        "balance": "$2,411.55",
+        "picture": "http://placehold.it/32x32",
+        "age": 40,
+        "eyeColor": "brown",
+        "name": "Claudia Mcmillan",
+        "gender": "female",
+        "company": "CONFRENZY",
+        "email": "claudiamcmillan@confrenzy.com",
+        "phone": "+1 (836) 581-3141",
+        "address": "101 Butler Place, Haena, New Jersey, 6697",
+        "about": "Nostrud sint sint velit tempor et laborum aute cupidatat anim ut cupidatat nostrud fugiat. Veniam sint nulla velit ut in ut nostrud aute culpa anim nulla dolore sint commodo. Ipsum ut officia dolore tempor. Eu cillum laborum aute non id pariatur consequat et do voluptate. Sit ullamco velit non velit consectetur excepteur sunt quis cillum Lorem. Incididunt do excepteur anim mollit sunt reprehenderit consectetur sint eiusmod sint cillum enim.\r\n",
+        "registered": "2014-09-28T11:25:36 -08:00",
+        "latitude": -10.908081,
+        "longitude": -122.167078,
+        "tags": [
+            "sit",
+            "veniam",
+            "ipsum",
+            "nulla",
+            "pariatur",
+            "aliqua",
+            "consectetur"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Reed Griffith"
+            },
+            {
+                "id": 1,
+                "name": "Jackson Fry"
+            },
+            {
+                "id": 2,
+                "name": "Kristie Franco"
+            }
+        ],
+        "greeting": "Hello, Claudia Mcmillan! You have 8 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ee8800381e8e933e49e0ff0",
+        "index": 5,
+        "guid": "b0566ac1-0f15-4feb-a35a-4070e3ddfeaf",
+        "isActive": true,
+        "balance": "$3,675.86",
+        "picture": "http://placehold.it/32x32",
+        "age": 20,
+        "eyeColor": "brown",
+        "name": "Atkins Hahn",
+        "gender": "male",
+        "company": "BITENDREX",
+        "email": "atkinshahn@bitendrex.com",
+        "phone": "+1 (960) 599-2933",
+        "address": "848 Eagle Street, Kidder, Arkansas, 3829",
+        "about": "Est sint ullamco pariatur sint. Fugiat quis pariatur id id id dolor. Sunt eu culpa in incididunt sunt in consequat velit. Officia mollit est duis et tempor. Est ipsum cillum ex non sit cupidatat nostrud id nulla culpa. Elit nostrud deserunt sunt non minim sit cupidatat est. Officia Lorem laboris fugiat exercitation adipisicing id reprehenderit dolore anim dolore enim.\r\n",
+        "registered": "2019-05-17T07:09:49 -08:00",
+        "latitude": 81.398641,
+        "longitude": -98.760767,
+        "tags": [
+            "incididunt",
+            "sunt",
+            "labore",
+            "fugiat",
+            "do",
+            "id",
+            "minim"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shields Yates"
+            },
+            {
+                "id": 1,
+                "name": "Lacy Russo"
+            },
+            {
+                "id": 2,
+                "name": "Charles Warren"
+            }
+        ],
+        "greeting": "Hello, Atkins Hahn! You have 7 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5ee880038cffaca467af457c",
+        "index": 6,
+        "guid": "6985a657-3cef-4e85-bd64-e5c896df3b04",
+        "isActive": true,
+        "balance": "$2,591.67",
+        "picture": "http://placehold.it/32x32",
+        "age": 32,
+        "eyeColor": "blue",
+        "name": "Garrison Blackburn",
+        "gender": "male",
+        "company": "QUALITERN",
+        "email": "garrisonblackburn@qualitern.com",
+        "phone": "+1 (855) 595-2680",
+        "address": "387 Tiffany Place, Barclay, Northern Mariana Islands, 6716",
+        "about": "Ipsum ea et ad laborum ea deserunt do magna cupidatat do nisi. Sunt minim proident et laboris labore est officia do ipsum elit. Culpa cillum nulla commodo anim elit. Cillum nisi dolor consectetur proident ipsum ad laborum est elit Lorem ad laboris ad. Fugiat quis occaecat laborum consequat adipisicing fugiat nostrud do dolore dolore et.\r\n",
+        "registered": "2014-12-15T01:21:54 -08:00",
+        "latitude": 81.031642,
+        "longitude": 6.855271,
+        "tags": [
+            "esse",
+            "reprehenderit",
+            "eu",
+            "laboris",
+            "veniam",
+            "cillum",
+            "fugiat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Noemi Melendez"
+            },
+            {
+                "id": 1,
+                "name": "Pollard Merritt"
+            },
+            {
+                "id": 2,
+                "name": "Gordon Parks"
+            }
+        ],
+        "greeting": "Hello, Garrison Blackburn! You have 9 unread messages.",
+        "favoriteFruit": "apple"
+    }
+];
